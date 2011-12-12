0218e7212e3eda23c4ba682db7e50f90a2e723db
Doug Johnston <dbjohnston@gmail.com>
2011-12-12 14:39:57 -0800

    It actually works now, but should support multiple lines, etc.

--- lib/sneakers.rb
+++ lib/sneakers.rb
@@ -2,26 +2,65 @@ class Sneakers
   def initialize
     puts 'Enter a phrase to sneakerize:'
     @phrase = gets.chomp
+    @length = @phrase.length
     @control = Array.new(@phrase.size, 0)
   end
 
   def display
+    system('clear')
     sneakerize
+    puts
   end
 
   def sneakerize
-    puts
-    100.times do
-      print (0...@phrase.length).collect { random_char }.join
+    Thread.new do
+      sleep 2
+      loop do
+        index = Kernel.rand(@length)
+        blank = @control[index]
+        if blank == 0
+          @control[index] = 1
+          finish if @control.include?(0) == false
+          sleep 0.1
+        end
+      end
+    end
+
+    loop do
+      print_line
       sleep 0.1
+    end
+  end
+
+  def print_line(overwrite=true)
+    print output
+    if overwrite
       @phrase.length.times do
         print "\x08"
       end
     end
+  end
+
+  def finish
+    print_line(false)
     puts
+    puts "Done!"
+    exit
   end
 
 private
+  def output
+    out = ""
+    @control.each_with_index do |c, i|
+      if c == 0
+        out += random_char
+      else
+        out += @phrase[i]
+      end
+    end
+    out
+  end
+
   def random_char
     (45..126).to_a[Kernel.rand(81)].chr
   end
